45507f96f3a7d3bba5c028e38eb694d0b76d0a30
lifeNewbi <lastchallenged@gmail.com>
2022-10-06 21:49:39 +0900

    셀레니움 설정

--- main.py
+++ main.py
@@ -111,8 +111,8 @@ def kt_dict():
     webdriver_options.add_argument("--disable-setuid-sandbox")
     webdriver_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=webdriver_options)
-    driver = webdriver.Chrome(executable_path=r'/Users/10cm_mocha/Downloads/chromedriver', options=webdriver_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=webdriver_options)
+    # driver = webdriver.Chrome(executable_path=r'/Users/10cm_mocha/Downloads/chromedriver', options=webdriver_options)
     driver.get('https://event.kt.com/html/event/ongoing_event_list.html')
 
     # 암시적 대기 - 페이지가 로드될 때까지 기다리다가 실행해라.
@@ -161,8 +161,8 @@ def lg_dict():
     webdriver_options.add_argument("--disable-setuid-sandbox")
     webdriver_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=webdriver_options)
-    driver = webdriver.Chrome(executable_path=r'/Users/10cm_mocha/Downloads/chromedriver', options=webdriver_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=webdriver_options)
+    # driver = webdriver.Chrome(executable_path=r'/Users/10cm_mocha/Downloads/chromedriver', options=webdriver_options)
     driver.get('https://www.lguplus.com/benefit-event/ongoing')
 
     driver.implicitly_wait(10)
